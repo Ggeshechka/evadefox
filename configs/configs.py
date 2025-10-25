89d46f52313db9704bdd1e1b17029b2617c36a3f
Ggeshechka <ix.reylk@gmail.com>
2025-10-25 15:30:48 +0500

    all delete

--- configs/configs.py
+++
@@ -1,3 +0,0 @@
-class AppInfo:
-    NAME='Evadefox'
-    VERSION='0.0.1'
--- models/models.py
+++
@@ -1,9 +0,0 @@
-class LocalesCategory:
-    INTERFACE = 'interface'
-    ERRORS = 'errors'
-    SUCCESS = 'success'
-
-
-class Language:
-    RU = 'Русский'
-    EN = 'English'
